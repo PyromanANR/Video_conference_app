696137abb30da7f330c36df49f77b45a820f445e
Nikita <avdeevn1142@gmail.com>
2024-04-22 23:44:28 +0300

    add db

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Video_conference_app.Data;
+using System.IO;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<Video_conference_appContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Video_conference_appContext") ?? throw new InvalidOperationException("Connection string 'Video_conference_appContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(builder.Environment.ContentRootPath, "App_Data"));
+
 
 var app = builder.Build();
 
