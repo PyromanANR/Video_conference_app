2f9ef20f9bb1ee3befe5d7d3976136094a956957
Nikita <avdeevn1142@gmail.com>
2024-04-23 14:22:11 +0300

    add simple chat

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Video_conference_app.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult HostChat()
+        {
+            var hostId = Guid.NewGuid().ToString();
+            var requestScheme = Request.Scheme; // Get the current request scheme
+            ViewBag.HostId = hostId;
+            ViewBag.RequestScheme = requestScheme; // Pass the request scheme to the view
+            return View("HostView");
+        }
+
+        public IActionResult JoinChat(string hostId)
+        {
+            ViewBag.HostId = hostId;
+            return View("JoinView");
+        }
+    }
+}
