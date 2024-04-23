e71f43ce55b319d30fbe91117d5f0e3be327b193
Vlad Buzilov <vladbuzilov23@gmail.com>
2024-04-23 21:54:04 +0300

    -

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -6,23 +6,19 @@ namespace Video_conference_app.Controllers
     {
         public IActionResult HostChat()
         {
-            // Generate a unique room ID
             string roomId = Guid.NewGuid().ToString();
             return RedirectToAction("JoinChat", "Chat", new { roomId });
         }
 
         public IActionResult JoinChat(string roomId)
         {
-            // Validate the roomId parameter
             if (string.IsNullOrEmpty(roomId))
             {
                 return BadRequest("Invalid room ID");
             }
 
-            // Generate a link for the client to join the chat room
             var joinChatLink = Url.Action("JoinChat", "Chat", new { roomId }, Request.Scheme);
 
-            // You can pass additional data to the view if needed
             ViewBag.RoomId = roomId;
             ViewBag.JoinChatLink = joinChatLink;
 
