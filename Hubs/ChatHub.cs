aea927540ba6690afab8ec943487f38bbd2cee2c
Vlad Buzilov <vladbuzilov23@gmail.com>
2024-05-09 14:17:23 +0300

    refacroed video and chat

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -4,43 +4,23 @@ namespace Video_conference_app.Hubs
 {
     public class ChatHub : Hub
     {
-        private static ConcurrentDictionary<string, List<string>> messageHistory = new ConcurrentDictionary<string, List<string>>();
-
-        public async Task SendMessage(string roomId, string user, string message)
+        public async Task JoinRoom(string roomId, string userId)
         {
-            var fullMessage = $"{user}: {message}";
-
-            await Clients.Group(roomId).SendAsync("ReceiveMessage", user, message);
-
-            messageHistory.AddOrUpdate(roomId, new List<string> { fullMessage }, (key, oldList) => {
-                oldList.Add(fullMessage);
-                return oldList;
-            });
+            Users.list.Add(Context.ConnectionId, userId);
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+            await Clients.Groups(roomId).SendAsync("user-connected", userId);
         }
 
-        public async Task JoinRoom(string roomId, string personId)
-        {
-            var connectionId = Context.ConnectionId;
-            await Groups.AddToGroupAsync(connectionId, roomId);
-
-            if (messageHistory.TryGetValue(roomId, out var messages))
-            {
-                await Clients.Client(connectionId).SendAsync("ReceiveMessageHistory", messages);
-            }
-            
-        }
-
-        public async Task LeaveRoom(string roomId)
-        {
-            var connectionId = Context.ConnectionId;
-
-            await Groups.RemoveFromGroupAsync(connectionId, roomId);
-        }
-        
         public override Task OnDisconnectedAsync(Exception? exception)
         {
             Clients.All.SendAsync("user-disconnected", Users.list[Context.ConnectionId]);
             return base.OnDisconnectedAsync(exception);
+        }
+    
+        // Add this method to your SignalR hub class
+        public async Task SendMessage(string roomId, string senderId, string message)
+        {
+            await Clients.Groups(roomId).SendAsync("ReceiveMessage", senderId, message);
         }
     }
 }
